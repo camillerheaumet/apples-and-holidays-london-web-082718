b63cd026220be83bdeb1e2184c0787c7115ed792
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-29 16:33:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supply|
       
-      puts "#{season.to_s}:"
+      puts "#{season.to_s.capitalize}:"
       puts "#{holiday.to_s}: #{supply}"
      
       #holiday_hash = season.to_s, holiday.to_s.capitalize!, [supply.to_s]
